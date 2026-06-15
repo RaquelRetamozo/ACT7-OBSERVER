b3b9cbbbfd17d2df1f4f3567a073fe51f76aa03d
RAQUEL <retamozoutaniraquel@gmail.com>
2026-06-14 21:12:58 -0500

    Modificación

--- src/EventManager.java
+++ src/EventManager.java
@@ -1,8 +1,12 @@
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class EventManager {
     private HashMap<String, EventListener> listeners = new HashMap<>();
+    //Agrega un pool de hilos reutilizables
+    private ExecutorService executor = Executors.newCachedThreadPool();
 
     public void subscribe(String eventType, EventListener listener){
         listeners.put(eventType, listener);
@@ -14,13 +18,17 @@ public class EventManager {
 
     public void notify(String eventType, String data){
         listeners.forEach((et, lstr) ->
-                {
+                executor.submit(() -> {   //Agregado envuelve en executor.submit cada listener se notifica en un hilo separado de dorme asincrona
                     try {
                         lstr.update(data);
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
-                }
+                })
         );
+    }
+    //Metodo Agregado que cierra el pool de hilos ordenadamente ya no se necesita
+    public void shutdown(){
+        executor.shutdown();
     }
 }
---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Hola usil");
+        Editor editor = new Editor();
+
+        LoggingListener logger = new LoggingListener("log.txt", "Hola Logging");
+        editor.events.subscribe("open", logger);
+
+        EmailAlertsListener emailAlerts = new EmailAlertsListener("rjru@gmail.com", "Esto debe cambiar");
+        editor.events.subscribe("save", emailAlerts);
+
+        editor.openFile("C:/Users/RAQUEL/Downloads/PatronObserver/doc.txt");
+        editor.saveFile();
+        editor.openFile("C:/Users/RAQUEL/Downloads/PatronObserver/doc.txt");
+        editor.saveFile();
+        editor.events.shutdown(); // Agregado
+    }
+}
